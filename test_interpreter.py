2e186c85fffd85904a25de7ec1086f66d8c413e9
Byoung Gi Lee <handrake@gmail.com>
2015-11-01 12:52:25 +0900

    Put spaces between class methods

--- test_interpreter.py
+++ test_interpreter.py
@@ -6,7 +6,9 @@ hello_case = ("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++.
 class InterpreterTestCase(unittest.TestCase):
     def setUp(self):
         self.interpreter = brainfuck.BrainfuckInterpreter()
+
     def test_hello_world(self):
         self.assertEqual(hello_case[1], self.interpreter.eval(hello_case[0]))
+
     def test_missing_parenthesis(self):
         self.assertRaises(SyntaxError, self.interpreter.eval, '[++]+]')
